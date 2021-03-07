ff3bfcd8c4ef4cbb248d6136526fe8d5989e7cac
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-07 11:50:01 -0800

    Deleted much of old header comment

--- bmp180-esp8266.ino
+++ bmp180-esp8266.ino
@@ -1,28 +1,14 @@
 // Modified for ESP8266 - Brian Ernst Feb 2021
+// Added MQTT and WIFI code - Brian Ernst March 2021
 
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
-   
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-   
+/*
    Connections for ESP 8266
    ===========
    Connect SCL to D1
    Connect SDA to D2
    Connect VDD to 3.3V DC
    Connect GROUND to common ground
-    
-   History
-   =======
-   2013/JUN/17  - Updated altitude calculations (KTOWN)
-   2013/FEB/13  - First version (KTOWN)
-*/
+ */
 
 // INCLUDE
 
