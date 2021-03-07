f98d6af9ea7bcca41d27387431f9a86ce13ba34a
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-06 19:10:21 -0800

    add mqtt callback function and setup code

--- bmp180-esp8266.ino
+++ bmp180-esp8266.ino
@@ -99,6 +99,26 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
 /**************************************************************************/
 /*
     Arduino setup function (automatically called at startup)
@@ -122,6 +142,10 @@ void setup(void)
   
 // Setup the Wifi connection
   setup_wifi();
+
+// Setup MQTT client
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
 }
 
