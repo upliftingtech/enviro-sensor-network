14ddaa4fe85603756fad37e320b7f51a3146227e
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-06 19:12:50 -0800

    get rid of callback function. not needed for this project at this time

--- bmp180-esp8266.ino
+++ bmp180-esp8266.ino
@@ -99,25 +99,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
 
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
-}
 
 /**************************************************************************/
 /*
@@ -145,7 +127,6 @@ void setup(void)
 
 // Setup MQTT client
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
 
 }
 
