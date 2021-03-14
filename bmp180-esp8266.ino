81ffcb2cde902f360e76a935cab2234dc1089d59
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-13 23:03:27 -0800

    hardcode topic to get it to compile
    get it compiling. clientId isn't of a type publish can use

--- bmp180-esp8266.ino
+++ bmp180-esp8266.ino
@@ -166,7 +166,7 @@ void getSensorEvent()
         snprintf (msg, MSG_BUFFER_SIZE, "temp: %3.1f C", temperature);
         Serial.print("Publish message: ");
         Serial.println(msg);
-	    client.publish(clientId, msg); // use clientID for topic
+	    client.publish("sensor01", msg); // to do: use clientID for topic
 
 	  }
 	  else
