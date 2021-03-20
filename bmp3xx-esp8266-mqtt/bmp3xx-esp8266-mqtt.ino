b765f274789054243ab1e40f5ef6e709a2a540a5
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-20 13:07:29 -0700

    add folders for ino files
    create folders the way the arduino IDE likes. add to bmp3xx stub. remove bmp3xx example code.

---
+++ bmp3xx-esp8266-mqtt/bmp3xx-esp8266-mqtt.ino
@@ -0,0 +1,26 @@
+
+#include <Streaming.h>
+#include <Chrono.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+
+// Instantiate a Chrono object.
+Chrono timeToSample;
+
+void setup()
+{
+	Serial.begin(115200);
+	Serial << endl << "Get data from a BMP3xx sensor!" << endl;
+}
+
+void loop()
+{
+  // every 10 seconds take a sample and output to mqtt
+
+  if (timeToSample.hasPassed(10000))
+  {
+    Serial << "this would be a sample..." << endl;
+  }
+}
