9875b5bdda532c8e0d28bcb266d1b6ed29567c52
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-20 12:47:49 -0700

    Create bmp3xx-esp8266-mqtt.ino
    start code from scratch for this sensor

---
+++ bmp3xx-esp8266-mqtt.ino
@@ -0,0 +1,16 @@
+
+#include <streaming.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+
+void setup()
+{
+	Serial.begin(115200);
+	Serial << 
+}
+
+void loop()
+{
+}
