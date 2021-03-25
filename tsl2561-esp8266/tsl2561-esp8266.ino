44ceb63fc318ad4f4d7fe3c4972e18f843bb79d6
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-24 18:52:26 -0700

    add setup and loop code

--- tsl2561-esp8266/tsl2561-esp8266.ino
+++ tsl2561-esp8266/tsl2561-esp8266.ino
@@ -1,3 +1,5 @@
+#include <Streaming.h>
+#include <Chrono.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_TSL2561_U.h>
@@ -29,22 +31,57 @@ void configureSensor(void)
   tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
   
   /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
-  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
   // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
-  // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
 
   /* Update these values depending on what you've set above! */  
   Serial.println("------------------------------------");
   Serial.print  ("Gain:         "); Serial.println("Auto");
-  Serial.print  ("Timing:       "); Serial.println("13 ms");
+  Serial.print  ("Timing:       "); Serial.println("402 ms. Slow 16-bit data.");
   Serial.println("------------------------------------");
 }
 
 
 void setup();
 {
+    Serial.begin(115200);
+    
+    Serial << endl << "Sensor test." << endl;
+    
+    if(!tsl.begin())
+    {
+      /* There was a problem detecting the TSL2561 ... check your connections */
+      Serial << "Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!" << endl;
+      while(1);
+    }
+
+    configureSensor();
+
 }
 
 void loop();
 {
+    sensors_event_t event;
+
+    if (timeToSample.hasPassed(10000))
+  {
+    // reset chrono timer
+    timeToSample.restart();
+    
+    // Get a new sensor event
+    tsl.getEvent(&event);
+    
+    /* Display the results (light is measured in lux) */
+    if (event.light)
+    {
+      Serial << event.light << " lux" << endl;
+    }
+    else
+    {
+      /* If event.light = 0 lux the sensor is probably saturated
+         and no reliable data could be generated! */
+      Serial << "Sensor overload" << endl;
+    }
+  }
 }
