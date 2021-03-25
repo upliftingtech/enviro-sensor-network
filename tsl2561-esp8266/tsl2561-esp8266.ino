c6b8416ac35351321c526f2128b0e32d975d333f
upliftingtech <77591525+upliftingtech@users.noreply.github.com>
2021-03-24 18:30:06 -0700

    declare lightSensor

--- tsl2561-esp8266/tsl2561-esp8266.ino
+++ tsl2561-esp8266/tsl2561-esp8266.ino
@@ -2,6 +2,16 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_TSL2561_U.h>
 
+// From Adafruit example code:
+// The address will be different depending on whether you leave
+// the ADDR pin float (addr 0x39), or tie it to ground or vcc. In those cases
+// use TSL2561_ADDR_LOW (0x29) or TSL2561_ADDR_HIGH (0x49) respectively
+Adafruit_TSL2561 lightSensor = Adafruit_TSL2561(TSL2561_ADDR_FLOAT, 12345);
+
 void setup();
+{
+}
 
 void loop();
+{
+}
